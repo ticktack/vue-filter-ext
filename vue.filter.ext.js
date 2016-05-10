fb4378b3b6c302866b5ce760dfb2e0c2c3021322
ticktack <yfq2010@gmail.com>
2016-05-10 15:05:13 +0800

    Update vue.filter.ext.js

--- vue.filter.ext.js
+++ vue.filter.ext.js
@@ -7,7 +7,7 @@ Vue.filter('dateFormat', function (timeMillis, formatString) {
 	if (formatString == undefined) {
 		formatString = 'yyyy-MM-dd HH:mm:ss'
 	}
-	 var dtime =  format(timeMillis, formatString)
+	var dtime =  format(timeMillis, formatString)
 	 
 	//计算出相差天数
 	var days = Math.floor(date/(24*3600*1000))
@@ -23,41 +23,41 @@ Vue.filter('dateFormat', function (timeMillis, formatString) {
 	//计算相差秒数
 	var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
 	var seconds = Math.round(leave3/1000)
-	var formatDtime = dtime;
+	var formatDtime = dtime
 	
-		if (days == 0 && hours == 0) {
-			if (mins < 1) {
-				formatDtime = seconds + "秒前";
-			} else {
-				formatDtime = mins + "分钟前";
+	if (days == 0 && hours == 0) {
+		if (mins < 1) {
+			formatDtime = seconds + "秒前"
+		} else {
+			formatDtime = mins + "分钟前"
+		}
+	} 
+	else {
+		if (days == 0 && hours != 0) {
+			formatDtime = hours + "小时前"
+		} else {
+			if (days < 7) {
+				formatDtime = days + "天前"
 			}
-		} 
-		else {
-			if (days == 0 && hours != 0) {
-				formatDtime = hours + "小时前";
-			} else {
-				if (days < 7) {
-					formatDtime = days + "天前";
+			// 当前年份
+			else {
+				if (now.getFullYear().toString() == dtime.substring(0, 4)) {
+					formatDtime = dtime.substring(5)
 				}
-				// 当前年份
+				else if (now.getFullYear().toString()-1 == dtime.substring(0, 4)) {
+					formatDtime = "去年" + dtime.substring(5)
+				}
 				else {
-					if (now.getFullYear().toString() == dtime.substring(0, 4)) {
-						formatDtime = dtime.substring(5);
-					}
-					else if (now.getFullYear().toString()-1 == dtime.substring(0, 4)) {
-						formatDtime = "去年" + dtime.substring(5);
-					}
-					else {
-						formatDtime = dtime;
-					}
+					formatDtime = dtime
 				}
 			}
 		}
+	}
 	
 	return  formatDtime
  }) 
  
 // format : 'yyyy-MM-dd HH:mm:ss'
 Vue.filter('dateFormat2', function (timeMillis, formatString) {
-		 return format(timeMillis, formatString);
+	return format(timeMillis, formatString)
 })
